92ab3d9f49d70df60b53a15248be6f3ae2978352
10900096220 <ade.cuellar92@gmail.com>
2025-03-19 12:14:30 -0600

    migration

--- Solution/WebApplication1/Domain/Entities/Movies.cs
+++ Solution/WebApplication1/Domain/Entities/Movies.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Domain.Entities
         public int MovieId { get; set; }
         public string MovieTitle { get; set; }
         public string? MovieDescription { get; set;}
+        public string MovieImage { get; set; }
         public int MovieRaiting { get; set;}
         public int CategoryId { get; set; }
         public int ActorId { get; set; }
---
+++ Solution/WebApplication1/Migrations/20250319174816_AddNewFieldToActor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFieldToActor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MovieImage",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MovieImage",
+                table: "Movies");
+        }
+    }
+}
